e58f1e973bea6455fd8272c0e60cd5416af6a2bb
BilalTaher97 <belalkhasawneh97@gmail.com>
2025-03-06 15:58:52 +0300

    Add Task

--- Task_1/Task1.js
+++ Task_1/Task1.js
@@ -1,3 +1,4 @@
+//------------------------------------ (1)
 var num = 31;
 console.log('\n\n\n');
 if (num % 2 == 0) {
@@ -7,12 +8,14 @@ else {
     console.log("Odd");
 }
 console.log('\n\n\n');
+//------------------------------------ (2)
 var Price = [2.45, 45.2, 12.3, 4.2, 55.3, 1, 6, 8, 10, 34.3, 77.5];
 for (var i = 0; i < Price.length; i++) {
     if (Price[i] >= 10) {
         console.log(Price[i]);
     }
 }
+//------------------------------------ (3)
 var Arr = [3, 5, 8, 5, 33, 11, 53, 76, 33, 12, 6];
 var Sum = 0;
 for (var i = 0; i < Arr.length; i++) {
